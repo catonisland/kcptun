82e0f5aaacebc049a3485cb09c324ff805fab418
Leonn <catonisland@users.noreply.github.com>
2016-11-05 15:07:12 +0800

    Create stop.sh

---
+++ stop.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+PID=`ps -ef | grep server_linux_amd64 | grep -v grep | awk '{print $2}'`
+if [ "" != "$PID" ]; then
+ echo "killing $PID"
+ kill -9 $PID
+else
+ echo "Kcptun not running!"
+fi
